c3bc49a4a729558059546f4234304de42e83d986
Benjamin <kacerovsky@gmail.com>
2023-03-12 23:23:29 -0400

    Start building MatlabModelLoader.cs

---
+++ Assets/Editor/MatlabModelLoader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JBK.AstrocyteViewer.Editor.Editor
+{
+    public class MatlabModelLoader : EditorWindow
+    {   
+        private string _xFigureFilePath;
+        
+        [MenuItem("JBK/MatlabModelLoader")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<MatlabModelLoader>();
+            window.titleContent = new GUIContent("MatlabModelLoader");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("XFigure File");
+            var filePath = EditorGUILayout.TextField(_xFigureFilePath);
+            if (GUILayout.Button("select file"))
+            { 
+                filePath = EditorUtility.OpenFilePanel("Select XFigure File", "~", "*mat");
+            }
+            if (!string.IsNullOrEmpty(filePath)) _xFigureFilePath = filePath;
+            
+            if (GUILayout.Button("Load Model")) LoadModel();
+        }
+
+        private void LoadModel()
+        {
+            Debug.Log(_xFigureFilePath);
+        }
+    }
+}
